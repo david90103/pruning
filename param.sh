7b4c91c4a7d89207b710ee5d6a760c4820fd7103
David Tsai <david90103@gmail.com>
2022-01-05 14:27:40 +0800

    update param search script

--- param.sh
+++ param.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 REGION=( 4 )
-SEARCHER=( 1 )
-SAMPLE=( 4 )
+SEARCHER=( 1 2 )
+SAMPLE=( 1 2 4 8 )
 C_THRE=( 0.5 )
 M_THRE=( 0.1 )
 
-dir=output/params/sem/run2
+dir=output/params/sem/run3
 
 
 mkdir -p ${dir}
@@ -14,18 +14,18 @@ mkdir -p ${dir}
 for r in "${REGION[@]}"
 do
     for sr in "${SEARCHER[@]}"
-    do  
+    do
         for sa in "${SAMPLE[@]}"
-        do  
+        do
             for c in "${C_THRE[@]}"
-            do  
+            do
                 for m in "${M_THRE[@]}"
-                do 
+                do
                     path=${dir}/sem_r${r}_se${sr}_sa${sa}_c${c}_m${m}.out
                     date > ${path}
 
                     mkdir -p ${dir}/${a}
-                    python resprune.py  --model "origin/resnet56/model_best.pth.tar"    \
+                    python resprune.py  --model "origin/cifar10/resnet56/model_best.pth.tar"    \
                                         --depth 56                                      \
                                         --save ${dir}/${a}/pruned                   \
                                         --region ${r}                               \
@@ -33,6 +33,7 @@ do
                                         --sample ${sa}                              \
                                         --mthre ${m}                                \
                                         --cthre ${c}                                \
+                                        --iter 300                                  \
                                         --algo sem                                  \
                                         >> ${path}
                 done
